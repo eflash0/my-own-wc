9afbe12066860fc175a0cc96bbfc5c9d0085aa2a
unknown <edwardarahi2002@gmail.com>
2025-01-07 16:27:19 +0100

    second commit

--- WC.java
+++ WC.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -11,13 +12,14 @@ public class WC {
     String[] options = {"-l","-w","-m","-c"}; 
     static Set<String> myOptions = new HashSet<String>();
     static Set<String> files = new HashSet<String>();
+    static HashMap<String,Long> answers = new HashMap<>();
     //num bytes
     static long numBytes(File  file){
         return file.length();
     }
 
     //num lines
-    static int numLines(File file){
+    static long numLines(File file){
         int c = 0;
         try{
             Scanner reader = new Scanner(file);
@@ -36,7 +38,7 @@ public class WC {
     }
  
     //num words
-    static int numWords(File file){
+    static long numWords(File file){
         int c = 0;
         String[] words;
         String s = "";
@@ -55,7 +57,7 @@ public class WC {
     }
 
     //num chars
-    static int numCharacters(File file){
+    static long numCharacters(File file){
         int c = 0;
         String s = "";
         try(BufferedReader br = new BufferedReader(new FileReader(file))){
@@ -71,15 +73,44 @@ public class WC {
         return c;
     }
 
+    static void optionsSelector(Set<String> options,File file){
+        for(String option : options){
+            switch (option) {
+                case "-l":
+                    answers.put("lines", numLines(file));
+                    break;
+            
+                case "-c":
+                    answers.put("bytes", numBytes(file));
+                    break;
+
+                case "-w":
+                    answers.put("words", numWords(file));
+                    break; 
+
+                case "-m":
+                    answers.put("words", numCharacters(file));
+                    break;
+                    
+                default:
+                    break;    
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String folderName = System.getProperty("user.dir");
         String fileName = "";
         if(args.length > 0){
             for(String s : args){
+                if(s.startsWith("-") && !files.isEmpty()){
+                    System.err.println("the file names must be at the end of the command");
+                    return;
+                }
                 if(s.startsWith("-"))
                     myOptions.add(s);
                 else    
-                    fileName = s;    
+                    files.add(s);    
             }
         }  
         if(fileName.isEmpty()) return;
