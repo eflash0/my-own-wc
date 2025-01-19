8190ea89a17a5618a6d2195e767703a583933f62
unknown <edwardarahi2002@gmail.com>
2025-01-19 03:35:17 +0100

    standard input

--- WC.java
+++ WC.java
@@ -3,11 +3,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class WC {
     static String[] options = {"-l","-w","-m","-c"}; 
@@ -15,94 +19,135 @@ public class WC {
     static Set<String> files = new HashSet<String>();
     static HashMap<String,Long> answers = new HashMap<>();
     //num bytes
-    static long numBytes(File  file){
-        return file.length();
+    static long numBytes(Object object) throws Exception{
+        if(object instanceof File){
+            File file = (File) object;
+            return file.length();
+        }
+        else if(object instanceof String){
+            String input = (String) object;
+            return input.getBytes(StandardCharsets.UTF_8).length;
+        }
+        return 0;
     }
 
     //num lines
-    static long numLines(File file){
+    static long numLines(Object object) throws Exception{
         int c = 0;
-        try{
-            Scanner reader = new Scanner(file);
-            while(reader.hasNextLine()){
-                reader.nextLine();
-                c++;    
+        if (object instanceof File) {
+            File file = (File) object;
+            try{
+                Scanner reader = new Scanner(file);
+                while(reader.hasNextLine()){
+                    reader.nextLine();
+                    c++;    
+                }
+            }
+            catch(FileNotFoundException ex){
+                System.out.println(ex);
+            }
+            catch(Exception ex){
+                System.out.println(ex);
             }
         }
-        catch(FileNotFoundException ex){
-            System.out.println(ex);
-        }
-        catch(Exception ex){
-            System.out.println(ex);
+        else if (object instanceof String) {
+            String input = (String) object;
+            return input.lines().count();
         }
         return c;
     }
  
     //num words
-    static long numWords(File file){
+    static long numWords(Object object) throws Exception{
         int c = 0;
         String[] words;
         String s = "";
-        try{
-            Scanner reader = new Scanner(file);
-            while(reader.hasNextLine()){
-                s += reader.nextLine()+"\n";   
+        if (object instanceof File) {
+            File file = (File) object;
+            try{
+                Scanner reader = new Scanner(file);
+                while(reader.hasNextLine()){
+                    s += reader.nextLine()+"\n";   
+                }
+                words = s.split("\\s+");
+                c+=words.length;  
             }
-            words = s.split("\\s+");
-            c+=words.length;  
+            catch(FileNotFoundException ex){
+                System.out.println(ex);
+            }
         }
-        catch(FileNotFoundException ex){
-            System.out.println(ex);
+        else if (object instanceof String) {
+            String input = (String) object;
+            words = input.split("\\s+");
+            return words.length;
         }
         return c;
     }
 
     //num chars
-    static long numCharacters(File file){
-        int c = 0;
+    static long numCharacters(Object object) throws Exception{
         String s = "";
-        try(BufferedReader br = new BufferedReader(new FileReader(file))){
-            String line;
-            while ((line = br.readLine()) != null) {
-                s += line;
+        if (object instanceof File) {
+            File file = (File) object;
+            try(BufferedReader br = new BufferedReader(new FileReader(file))){
+                String line;
+                while ((line = br.readLine()) != null) {
+                    s += line;
+                }
             }
-            c = s.length();
+            catch(IOException e){
+                System.out.println(e.getMessage());
+            }
         }
-        catch(IOException e){
-            System.out.println(e.getMessage());
+        else if (object instanceof String) {
+            String input = (String) object;
+            return input.length();
         }
-        return c;
+        return s.length();
     }
 
-    static void optionsSelector(Set<String> options,File file){
+    static void optionSelector(Set<String> options,Object object,StringBuilder sb) throws Exception{
         if(options.isEmpty()){
-            answers.put("lines", numLines(file));
-            answers.put("bytes", numBytes(file));
-            answers.put("words", numWords(file));
-            answers.put("chars", numCharacters(file));
+            sb.append("   "+numBytes(object)+"\t");
+            sb.append(numCharacters(object)+"\t");
+            sb.append(numWords(object)+"\t");
+            sb.append(numLines(object)+"\t");
             return;
         }
         for(String option : options){
             switch (option) {
                 case "-l":
-                    answers.put("lines", numLines(file));
+                    sb.append("   "+numLines(object)+"\t");
                     break;
             
                 case "-c":
-                    answers.put("bytes", numBytes(file));
+                    sb.append("   "+numBytes(object)+"\t");
                     break;
 
                 case "-w":
-                    answers.put("words", numWords(file));
+                    sb.append("   "+numWords(object)+"\t");
                     break; 
 
                 case "-m":
-                    answers.put("chars", numCharacters(file));
+                    sb.append("   "+numCharacters(object)+"\t");
                     break;
                     
                 default:
                     break;    
             }
+        }
+    }
+
+    static void optionSelectorStandardInput(Set<String> options,BufferedReader reader,StringBuilder sb) throws Exception{
+        String line;
+        int countWords = 0;
+        int countBytes = 0;
+        int countChars = 0;
+        int countLines = 0;
+        while ((line = reader.readLine()) != null) {
+            countLines++;
+            //i will pass in the methdos above Object for type and check whether the instance is
+            // a file or a bufferReader
         }
     }
 
@@ -118,8 +163,7 @@ public class WC {
                     else{
                         myOptions.add(s);
                     }    
-                }
-                
+                } 
             }
             else{
                 files.add(s);
@@ -131,20 +175,27 @@ public class WC {
     public static void main(String[] args) {
         boolean b = false;
         String folderName = System.getProperty("user.dir");
-        System.out.println(folderName);
-        
+        StringBuilder sb = new StringBuilder();
         b = argsProcessor(args);
-        if(b){
-            for(String fileName : files){
-                File file = new File(folderName+"\\"+fileName);
-
-                optionsSelector(myOptions, file);
-                for(String key : answers.keySet()){
-                    System.out.println(fileName + "\t" + answers.get(key));
+        if(b){   
+            try {
+                if(!files.isEmpty()){
+                    for(String fileName : files){
+                        File file = new File(folderName+"\\"+fileName);
+                        optionSelector(myOptions, file,sb);
+                        sb.append(fileName);   
+                    }
                 }
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String input = reader.lines().collect(Collectors.joining("\n"));
+                if (!input.isEmpty()) { // Check if there's input in the stdin
+                    optionSelector(myOptions, input, sb);
+                }
+                System.out.println(sb.toString());
+            } 
+            catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
-        
     }
-
 }
